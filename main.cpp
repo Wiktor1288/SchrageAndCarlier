a28f9eedba18a569f5dc6956d9a8e13c3ffc22cd
WiktorSpringer <wiktor1288@wp.pl>
2021-03-11 18:46:06 +0100

    supplementation Schrage's algorithm

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <memory>
+#include <vector>
 #include "RandomNumberGenerator.h"
 #include "Zadania.hpp"
 using namespace std;
-void AddTasksParametrs(RandomNumberGenerator Random_2, unique_ptr <Task[]> &Tasks_1, int size_1);
-int ObjectiveFunction(unique_ptr <Task[]> &Tasks_1, unique_ptr <int []> &TabOrder, int size_1);
+void AddTasksParametrs(RandomNumberGenerator Random_2, shared_ptr <Task[]> Tasks_1, int size_1);
+int ObjectiveFunction(shared_ptr <Task[]> Tasks_1, shared_ptr <int []> TabOrder, int size_1);
+int LookforMinR(vector<Task> TaskVector);
+int LookforMaxH(vector<Task> TaskVector);
+void ShowInformation(shared_ptr <Task[]> Tasks_1, shared_ptr <int []> TabOrder, int size_1);
+shared_ptr <int []>  SchrageAlgorithm(shared_ptr <Task[]> Tasks_1, shared_ptr <int []> TabOrder, int size_1);
+
+
+
 int main(){
 
     auto i{0}, Number{0}, SeedValue{0};
@@ -16,67 +24,29 @@ int main(){
     cin >> SeedValue;
 
     RandomNumberGenerator Random_1(SeedValue);
-    unique_ptr <Task[]> TaskTab(new Task[Number]);
-    unique_ptr <int []> TabOrder(new int[Number]);
+    shared_ptr <Task[]> TaskTab(new Task[Number]);
+    shared_ptr <int []> TabOrder(new int[Number]);
+    
     
     for(i=0; i<Number; i++)
         TabOrder[i]=i;
 
     AddTasksParametrs(Random_1, TaskTab, Number);
     auto Cmaxx=ObjectiveFunction(TaskTab,TabOrder,Number);
+    ShowInformation(TaskTab,TabOrder,Number);
+    cout << endl << endl << "Cmax:  " << Cmaxx << endl;
+    TabOrder=SchrageAlgorithm(TaskTab,TabOrder,Number);
+    Cmaxx=ObjectiveFunction(TaskTab,TabOrder,Number);
+    ShowInformation(TaskTab,TabOrder,Number);
+    cout << endl << endl << "Cmax:  " << Cmaxx << endl;
 
-    cout << "ID:  ";
-    for ( i=0; i<Number; i++){
-        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('I') << " | ";
-    }
-
-    cout << endl << "r:  ";
-    for ( i=0; i<Number; i++){
-        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('P') << " | ";
-    }
-
-    cout << endl << "p:  ";
-    for ( i=0; i<Number; i++){
-        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('E') << " | ";
-    }
-
-    cout << endl << "q:  ";
-    for ( i=0; i<Number; i++){
-        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('H') << " | ";
-    }
-
-    cout << endl << "S:  ";
-    for ( i=0; i<Number; i++){
-        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('M') << " | ";
-    }
-
-    cout << endl << "C:  ";
-    for ( i=0; i<Number; i++){
-        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('T') << " | ";
-    }
-
-    cout << endl << "C+q:  ";
-    for ( i=0; i<Number; i++){
-        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('T')+TaskTab[TabOrder[i]].ShowValueOfVariable('H') << " | ";
-    }
-
-    cout << endl << Cmaxx;
-    
-    
-    
-
-
-
-
-
-
-
+    cout <<endl;
     return 0;
 }
 
 
 
-void AddTasksParametrs(RandomNumberGenerator Random_2, unique_ptr <Task[]> &Tasks_1, int size_1)
+void AddTasksParametrs(RandomNumberGenerator Random_2, shared_ptr <Task[]> Tasks_1, int size_1)
 {
     auto i{0}, A{0}, X{29};
 
@@ -89,11 +59,11 @@ void AddTasksParametrs(RandomNumberGenerator Random_2, unique_ptr <Task[]> &Task
         Tasks_1[i].AddTPripare(Random_2.nextInt(1,A));
     }
     for ( i = 0; i < size_1; i++){
-        Tasks_1[i].AddTHandober(Random_2.nextInt(1,X));
+        Tasks_1[i].AddTHandober(Random_2.nextInt(1,A));
     }
 }
 
-int ObjectiveFunction(unique_ptr <Task[]> &Tasks_1, unique_ptr <int []> &TabOrder_1, int size_1){
+int ObjectiveFunction(shared_ptr <Task[]> Tasks_1, shared_ptr <int []> TabOrder_1, int size_1){
     auto i{0}, Cmax{0};
 
     Tasks_1[TabOrder_1[0]].AddStartMoment(Tasks_1[TabOrder_1[0]].ShowValueOfVariable('P'));
@@ -106,4 +76,119 @@ int ObjectiveFunction(unique_ptr <Task[]> &Tasks_1, unique_ptr <int []> &TabOrde
     }
 
     return Cmax;
+}
+
+
+void ShowInformation(shared_ptr <Task[]> TaskTab, shared_ptr <int []> TabOrder, int size_1){
+    auto i{0};
+        cout << "ID:  ";
+    for ( i=0; i<size_1; i++){
+        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('I') << " | ";
+    }
+
+    cout << endl << "r:  ";
+    for ( i=0; i<size_1; i++){
+        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('P') << " | ";
+    }
+
+    cout << endl << "p:  ";
+    for ( i=0; i<size_1; i++){
+        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('E') << " | ";
+    }
+
+    cout << endl << "q:  ";
+    for ( i=0; i<size_1; i++){
+        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('H') << " | ";
+    }
+
+    cout << endl << "S:  ";
+    for ( i=0; i<size_1; i++){
+        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('M') << " | ";
+    }
+
+    cout << endl << "C:  ";
+    for ( i=0; i<size_1; i++){
+        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('T') << " | ";
+    }
+
+    cout << endl << "C+q:  ";
+    for ( i=0; i<size_1; i++){
+        cout << TaskTab[TabOrder[i]].ShowValueOfVariable('T')+TaskTab[TabOrder[i]].ShowValueOfVariable('H') << " | ";
+    }
+
+}
+
+
+shared_ptr <int []>  SchrageAlgorithm(shared_ptr <Task[]> Tasks_1, shared_ptr <int []> TabOrder, int size_1){
+
+    auto k{0}, t{0}, i{1}, rmin{0},j{0};
+    vector <Task> Gset;
+    vector <Task> Nset;
+    
+    
+    for (i=0; i<size_1; i++){
+        Nset.push_back(Tasks_1[i]);
+    }
+
+
+    
+    t=Nset[LookforMinR(Nset)].ShowValueOfVariable('P');
+    
+
+    while(Gset.size()!=0 || Nset.size()!=0){
+        while( Nset.size()!=0 && Nset[LookforMinR(Nset)].ShowValueOfVariable('P')<=t){
+            j=LookforMinR(Nset);
+            Gset.push_back(Nset[j]);
+            Nset.erase(Nset.begin()+j);
+        }
+        if(Gset.size()!=0){
+            j=LookforMaxH(Gset);
+            TabOrder[k]=Gset[j].ShowValueOfVariable('I')-1;
+            t+=Gset[j].ShowValueOfVariable('E');
+            Gset.erase(Gset.begin()+j);
+            k+=1;
+        }
+        else{
+            t=Nset[LookforMinR(Nset)].ShowValueOfVariable('P');
+        }
+
+    }
+
+
+    return TabOrder;
+
+}
+
+
+int LookforMinR(vector<Task> TaskVector){
+    auto i{0}, minValue{0},x{0};
+    minValue=TaskVector[0].ShowValueOfVariable('P');
+    for(i=0; i<TaskVector.size(); i++)
+       minValue = min(minValue,TaskVector[i].ShowValueOfVariable('P'));
+
+    for(i=0; i<TaskVector.size(); i++){
+        if(TaskVector[i].ShowValueOfVariable('P')==minValue){
+            x=i;
+            break;
+        }
+            
+    }
+
+    return x;
+}
+
+int LookforMaxH(vector<Task> TaskVector){
+    auto i{0}, maxValue{0},x{0};
+    maxValue=TaskVector[0].ShowValueOfVariable('H');
+    for(i=1; i<TaskVector.size(); i++)
+       maxValue = max(maxValue ,TaskVector[i].ShowValueOfVariable('H'));
+
+    for(i=0; i<TaskVector.size(); i++){
+        if(TaskVector[i].ShowValueOfVariable('H')==maxValue){
+            x=i;
+            break;
+        }
+    }
+
+    return x;
 }
